f7abcba8f09ea2582bf266a2052a5e5b0e5fef26
Predrag Stefanovic <predrag.stefanovic98@gmail.com>
2020-08-08 00:52:15 +0200

    Adding Vets page and controller. Closes #12

---
+++ pet-clinic-web/src/main/java/com/predrag/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.predrag.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+
+    @GetMapping({"/vets", "/vets/index"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
