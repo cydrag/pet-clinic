5cd4adf5c0feafd494843490924484aaa15c6abe
Predrag Stefanovic <predrag.stefanovic98@gmail.com>
2020-08-12 23:50:05 +0200

    VetController now lists all vets. Closes #15

--- pet-clinic-web/src/main/java/com/predrag/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/predrag/controllers/VetController.java
@@ -1,13 +1,26 @@
 package com.predrag.controllers;
 
+import com.predrag.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "/vets/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
